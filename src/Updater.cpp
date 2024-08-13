00a1c30c0135f8fcf2a4544ee99147cd9f2a8420
SmartSolutionsLabs <163151140+SmartSolutionsLabs@users.noreply.github.com>
2024-08-13 15:06:49 -0500

    check for valid version method

--- src/Updater.cpp
+++ src/Updater.cpp
@@ -64,6 +64,68 @@ bool GithubFirmwareUpdater::firmwareVersionCheck(void) {
         client->setInsecure();
         // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
         HTTPClient https;
+        https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+        if (https.begin( * client, fwurl)){ // HTTPS
+            Serial.print("[HTTPS] GET...\n");
+            // start connection and send HTTP header
+            vTaskDelay(1000);
+            httpCode = https.GET();
+            vTaskDelay(1000);
+            if (httpCode == HTTP_CODE_OK){ // if version received
+                payload = https.getString(); // save received version
+            }
+            else{
+                Serial.print("error in downloading version file:");
+                Serial.println(httpCode);
+            }
+            https.end();
+        }
+        delete client;
+    }
+
+    if (httpCode == HTTP_CODE_OK){
+        //payload.trim();
+        deserializeJson(this->getPayload,payload);
+        String msg = this->getPayload["lastest"];
+        //String msg = this->getPayload["versions"][3];
+        this->newFirmwareVersion = msg;
+
+        Serial.print("\t --------------- \n \t New Version Get: ");
+        Serial.println(this->newFirmwareVersion);
+        Serial.print("\t --------------- \n");
+
+        if (payload.equals(this->firmwareVersion)) {
+            Serial.printf("\nDevice already on latest firmware version:%s\n", this->firmwareVersion);
+            this->newFirmwareVersion = payload;
+            return false;
+        }
+        else
+        {
+            Serial.println(this->newFirmwareVersion);
+            Serial.println("New firmware detected");
+            return true;
+        }
+    }
+    return true;
+}
+
+bool GithubFirmwareUpdater::checkForValidVersion(void){
+    String payload;
+    int httpCode;
+    String fwurl = "";
+    fwurl += this->URL_check_Version;
+    fwurl += this->newFirmwareVersion;
+    fwurl += "/updateVersionURL.json";
+    fwurl += "?";
+    fwurl += String(rand());
+    Serial.println(fwurl);
+    WiFiClientSecure * client = new WiFiClientSecure;
+
+    if(client){
+    //client->setCACert(rootCACertificate);
+        client->setInsecure();
+        // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
+        HTTPClient https;
 
         if (https.begin( * client, fwurl)){ // HTTPS
             Serial.print("[HTTPS] GET...\n");
